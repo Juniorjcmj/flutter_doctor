71dd5a064bd6a5dadd63bd97d20e5c8cdeb94cfb
Jose Carlos Maria Junior <jotacfs2010@hotmail.com>
2023-09-09 20:52:30 -0300

    Complementando arquivo de configuração

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -23,4 +23,27 @@ class Config {
   static const spaceSmall = SizedBox(height: 25,);
   static final spaceMedium = SizedBox(height: screenHeight! * 0.05,);
   static final spaceBig = SizedBox(height: screenHeight! * 0.08,);
+
+
+
+  //textForm field border
+  static const outlineBorder = OutlineInputBorder(
+         borderRadius: BorderRadius.all(Radius.circular(8))
+  );
+
+  static const fucusBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+      borderSide: BorderSide(
+        color: Colors.greenAccent
+      )
+
+  );
+
+   static const errorBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(8)),
+      borderSide: BorderSide(
+        color: Colors.red
+      )
+
+  );
 }
