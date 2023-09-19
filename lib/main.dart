bf85ce0efcd980384001a0621da9cbd78a1d699b
Jose Carlos Maria Junior <jotacfs2010@hotmail.com>
2023-09-19 19:20:51 -0300

    configurando a navegação com getX

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
+
 import 'package:flutter_doctor/main_layout.dart';
 import 'package:flutter_doctor/screens/alterar_email.dart';
-import 'package:flutter_doctor/screens/alterar_senha.dart';
 import 'package:flutter_doctor/screens/atendimento_page.dart';
+import 'package:flutter_doctor/screens/calendar_page.dart';
 import 'package:flutter_doctor/screens/descontinuado/auth_page.dart';
 import 'package:flutter_doctor/screens/descontinuado/calendar_app.dart';
-import 'package:flutter_doctor/screens/calendar_page.dart';
 import 'package:flutter_doctor/screens/esqueci_senha_page.dart';
 import 'package:flutter_doctor/screens/login_auth.dart';
 import 'package:flutter_doctor/screens/perfil_page.dart';
 import 'package:flutter_doctor/utils/config.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       title: 'Fluter Doctor Demo',
@@ -53,17 +54,16 @@ class MyApp extends StatelessWidget {
         )
       ),
       initialRoute: AuthPage.routName,
-      routes: {
-        '/':(context) =>  LoginPage(),
-        MainLayout.routName:(context) =>const  MainLayout(),
-        AtendimentoPage.routName:(context) => const AtendimentoPage(),
-        TableCalendarPage.routName:(context) => const TableCalendarPage(),
-        CalendarApp.routNamd:(context) => const CalendarApp(),
-        RecuperarSenha.routNamed:(context) =>  RecuperarSenha(),
-        AlterarSenha.routNamed:(context) => AlterarSenha(),
-        Perfil.routNamed:(context) => Perfil(),
-        AlterarEmail.rounNamed :(context) => const AlterarEmail()
-      },   
+      getPages: [
+        GetPage(name: '/', page: () => const  LoginPage()),
+        GetPage(name:  MainLayout.routName, page: () =>const  MainLayout()),
+        GetPage(name: AtendimentoPage.routName, page: () => const AtendimentoPage()),
+        GetPage(name: TableCalendarPage.routName, page:() => const TableCalendarPage()),
+        GetPage(name: CalendarApp.routNamd, page: () => const CalendarApp(),),
+        GetPage(name: RecuperarSenha.routNamed, page: () => RecuperarSenha()),
+        GetPage(name: Perfil.routNamed, page: () =>  Perfil()),
+        GetPage(name: AlterarEmail.rounNamed, page: () => const AtendimentoPage(),)        
+      ],   
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
