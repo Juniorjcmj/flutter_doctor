fb78d6dcfe072e558c001489f8e120d70718cdb3
Jose Carlos Maria Junior <jotacfs2010@hotmail.com>
2023-09-22 04:44:26 -0300

    limpando a página home

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -92,45 +92,24 @@ class _HomePageState extends State<HomePage> {
                 ),
                 Config.spaceSmall,
 
-                SizedBox(
-                  height: Config.height * 0.05,
-                  child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: List<Widget>.generate(medCat.length, (index) {
-                        return Card(
-                          margin: EdgeInsets.only(right: 20),
-                          color: Config.primaryColor,
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 15, vertical: 10),
-                            child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  FaIcon(
-                                    medCat[index]['icon'],
-                                    color: Colors.white,
-                                  ),
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                  Text(
-                                    medCat[index]['category'],
-                                    style: TextStyle(
-                                        fontSize: 16, color: Colors.white),
-                                  ),
-                                ]),
-                          ),
-                        );
-                      }
-                      )
-                      ),
-                ),
-                Config.spaceSmall,
-                Text(
-                  'Consultas',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
+               Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.person_add, size: 80, color: Config.secundColor),
+                      SizedBox(width: 10,),
+                      FaIcon(FontAwesomeIcons.solidCalendarPlus, size: 60, color: Config.secundColor),
+                      SizedBox(width: 10,),
+                      Icon(Icons.admin_panel_settings, size: 80,color: Config.secundColor),
+                      SizedBox(width: 10,),
+                      Icon(Icons.attach_money, size: 80,color: Config.secundColor),
+                     
+                    ],
+                  )
+                ],
+               ),
+           
+               
                 Config.spaceSmall,
                // AppoimentCard(),
                 Config.spaceSmall,
